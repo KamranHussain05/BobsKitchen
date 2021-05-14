f2e4da2fa49e8762f58035aebb93e6138f2f75f7
Will-Le870 <83032447+Will-Le870@users.noreply.github.com>
2021-05-13 22:31:30 -0700

    scorer
    changed engine of scorer

--- final_JavaCapstoneProject/src/final_JavaCapstoneProject/SandwichScorer.java
+++ final_JavaCapstoneProject/src/final_JavaCapstoneProject/SandwichScorer.java
@@ -2,58 +2,93 @@ package final_JavaCapstoneProject;
 
 public class SandwichScorer {
 	
-	private static int score; 
+	private  int score; 
 	
-	public static void grilledCheeseScore (String bread, String cheese) {
+	public  void grilledCheeseScore (String bread, String cheese, String sauce, String veg1,String veg2, String veg3, String meat) {
 		if (bread == "White Bread") {
 			score++;
 		}
 		if (cheese == "American") {
 			score++;
 		}
+		if (sauce == "none") {
+			score++;
+		}
+		if (veg1 == "none"&& veg2 == "none" && veg3 == "none") {
+			score++;
+		}
+		if (meat == "none") {
+			score++;
+		}
 		System.out.println(score);
 		
 	}
-	public static void banhMiScore (String bread, String sauce, String veggies, String meat) {
+	public  void banhMiScore (String bread, String sauce, String veg1, String veg2, String veg3, String meat) {
 		if (bread == "Baguette") {
-			score++;
+			score=score+2;
 		}
 		if (sauce == "Mayonaise") {
 			score++;
 		}
-		if (veggies == "Cucumber") {
+		if (veg1 == "Cucumber" && veg2 == "none" && veg3 == "none") {
 			score++;
 		}
-		if (meat == "beef") {
+		else if (veg2 == "Cucumber" && veg2 == "none" && veg3 == "none") {
+			score++;
+		}
+		else if (veg3 == "Cucumber" && veg2 == "none" && veg3 == "none") {
+			score++;
+		}
+		
+		if (meat == "beef" && veg2 == "none" && veg3 == "none") {
 			score++;
 		}
 		System.out.println(score);
 	}
-	public static void bLT (String bread, String sauce, String veg1, String veg2, String meat) {
+	public  void bLT (String bread, String sauce, String veg1, String veg2, String veg3, String meat) {
 		if (bread == "Garlic Bread") {
 			score++;
 		}
 		if (sauce == "Honey Mustard") {
 			score++;
 		}
-		if (veg1 == "Lettuce") {
+		if (veg1 == "Lettuce" && veg2 == "tomato" && veg3 == "none") {
 			score++;
 		}
-		if (veg2 == "tomato") {
+		else if (veg1 == "lettuce" && veg2 == "none" && veg3 == "tomato") {
+			score++;
+		}
+		else if (veg1 == "none" && veg2 == "tomato" && veg3 == "lettuce") {
+			score++;
+		}
+		else if (veg1 == "tomato" && veg2 == "lettuce" && veg3 == "none") {
+			score++;
+		}
+		else if (veg1 == "none" && veg2 == "lettuce" && veg3 == "tomato") {
+			score++;
+		}
+		else if (veg1 == "tomato" && veg2 == "none" && veg3 == "lettuce") {
 			score++;
 		}
 		if (meat == "bacon") {
-			score++;
+			score=score+2;
 		}
 		System.out.println(score);
 	}
+	/*
+	 * public void neopolitan (String sauce, String meat, String crust, String veg1,
+	 * String cheese) { if (sauce == "tomato sauce" ) { score++; } if (crust ==
+	 * "thin crust") { score++; } if (meat == "none") { score++; }
+	 * 
+	 * }
+	 */
 	
 	/*
 	 * public static void pizzaScorer (String cheese, String sauce, String dough) {
 	 * if (cheese == "Mozzarella") { score++; } if (sauce == "Tomato") { score++; }
 	 * if (dough == "Thin crust") { score++; } }
 	 */
-	public static String getScore() {
+	public String getScore() {
 		String answer = String.valueOf(score);
 		return answer;
 	}
